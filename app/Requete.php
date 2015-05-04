86500cc07f15bc372ff090106c683c7bd5d83c62
fzed51 <fzed51@users.noreply.github.com>
2015-05-04 17:05:14 +0200

    :up: modification de la classe Requete

--- app/Requete.php
+++ app/Requete.php
@@ -31,15 +31,31 @@ class Requete {
 		$this->client['IP'] = $_SERVER['REMOTE_ADDR'];
 		$this->client['PORT'] = $_SERVER['REMOTE_PORT'];
 		$this->client['METHODE'] = $_SERVER['REQUEST_METHOD'];
+		if (isset($_POST['_METHODE'])) {
+			if (in_array($_POST['_METHODE'], ['GET', 'POST', 'PUT', 'DELETE'])) {
+				$this->client['METHODE'] = $_POST['_METHODE'];
+			}
+		}
 		$this->client['URI'] = $_SERVER['REQUEST_URI'];
 	}
 
 	private function initInputData() {
 		var_dump($_GET);
 		var_dump($_POST);
+		var_dump($_COOKIE);
+		if (isset($_GET)) {
+			$this->input = array_merge($this->input, $_GET);
+		}
+		if (isset($_POST)) {
+			$this->input = array_merge($this->input, $_POST);
+		}
+		if (isset($_COOKIE)) {
+			$this->input = array_merge($this->input, $_COOKIE);
+		}
 	}
 
 	static public function serveur($param) {
+
 		return self::returnVal('serveur', $param);
 	}
 
@@ -51,14 +67,15 @@ class Requete {
 		return self::returnVal('input', $param);
 	}
 
-	static private function isInit() {
+	static private function getInstance() {
 		if (is_null(self::$_instance)) {
 			throw new Exception("La Requete n'est pas initialisée et ne peut pas retourner de données !");
 		}
+		return self::$_instance;
 	}
 
 	static private function returnVal($arrayName, $param) {
-		$requete = self::$_instance;
+		$requete = self::getInstance();
 		if (isset($requete->$arrayName[$param])) {
 			return $requete->$arrayName[$param];
 		}
