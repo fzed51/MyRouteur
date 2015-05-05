357e6176ed9baba694edd01615cb56f25b3e53fd
fzed51 <fzed51@users.noreply.github.com>
2015-05-05 15:37:39 +0200

    :up: ajout d'une nouvelle type de route pour test

--- core.php
+++ core.php
@@ -9,12 +9,23 @@ require './vendor/autoload.php';
 $requete = new Requete();
 
 Routeur::add((new Route('GET', '/', function() {
-	Echo "index<br><a href=\"" . Routeur::getUrl('bonjour', ['name' => 'Fabien']) . "\">bonjour Fabien</a>";
+	Echo "index<br>" .
+	"<a href=\"" . Routeur::getUrl('bonjour', ['name' => 'Fabien']) . "\">bonjour Fabien</a>" .
+	"<a href=\"" . Routeur::getUrl('user_do', ['action' => 'update', 'id' => 95]) . "\">bonjour Fabien</a>";
 }))->setName('home'));
 
 Routeur::add((new Route('GET', 'helo/{name}', function($name) {
 	Echo "bonjour $name<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
 }))->setName('bonjour'));
+
+Routeur::add(
+		(new Route('GET', 'user/{action}/{id}', function($id, $action) {
+			Echo "l'utilisateur n°$id fait $action<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
+		}))
+				->setName('user_do')
+				->setValidation('id', '[0-9]')
+				->setValidation('action', 'delete|edit')
+);
 
 Routeur::reparti($requete);
 
