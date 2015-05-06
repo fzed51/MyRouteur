b1db2d705a17cb47129e3073c7cff65c9aa037ba
fzed51 <fzed51@yahoo.fr>
2015-05-06 06:32:22 +0200

    :up: Ajout des constantes ROOT / WEBROOT

--- core.php
+++ core.php
@@ -4,11 +4,21 @@ use App\Requete;
 use App\Route;
 use App\Routeur;
 
+define('DS', DIRECTORY_SEPARATOR);
+define('WS', '/');
+define('ROOT', __DIR__ . DS);
+$tabUrl = explode(basename(ROOT), filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL));
+if (count($tabUrl) > 1) {
+	define('WEBROOT', $tabUrl[0] . basename(ROOT) . WS);
+} else {
+	define('WEBROOT', WS);
+}
+
 require './vendor/autoload.php';
 
 $requete = new Requete();
 
-Routeur::add((new Route('GET', '/', function() {
+Routeur::add((new Route('GET', '', function() {
 	Echo "index<br>" .
 	"<a href=\"" . Routeur::getUrl('bonjour', ['name' => 'Fabien']) . "\">bonjour Fabien</a>" .
 	"<a href=\"" . Routeur::getUrl('user_do', ['action' => 'update', 'id' => 95]) . "\">bonjour Fabien</a>";
