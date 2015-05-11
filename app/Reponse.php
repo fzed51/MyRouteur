e00a8047d090ab5948483c9394cfdb26de74a59c
fzed51 <fzed51@users.noreply.github.com>
2015-05-11 14:33:29 +0200

    :+1:  Gestion du retour
    Ajout de la classe Reponse

---
+++ app/Reponse.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App;
+
+/**
+ * Description of Reponse
+ *
+ * @author fabien.sanchez
+ */
+class Reponse {
+	//put your code here
+}
