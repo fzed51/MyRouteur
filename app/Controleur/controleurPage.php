17d37a9d10071770c7f7ab22cd78eef0db061f78
fzed51 <fzed51@users.noreply.github.com>
2015-05-15 10:55:40 +0200

    ﻿:up: remaniment pour éviter les dépendances et amélirer l'organisation du code

--- app/Controleur/controleurPage.php
+++ app/Controleur/controleurPage.php
@@ -8,18 +8,18 @@
 
 namespace App\Controleur;
 
-use App\Routeur;
+use App\Routeur\Routeur;
 
 /**
  * Description of controleurPage
  *
  * @author fabien.sanchez
  */
-class controleurPage extends Controleur {
+class ControleurPage extends Controleur {
 
-	function about() {
-		echo "<h1>A-propos de MyRoute</h1>" . PHP_EOL
-		. "<a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
-	}
+    function about() {
+        echo "<h1>A-propos de MyRoute</h1>" . PHP_EOL
+        . "<a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
+    }
 
 }
--- core.php
+++ core.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Requete;
-use App\Route;
-use App\Routeur;
+use App\Routeur\Routeur;
 
 define('DS', DIRECTORY_SEPARATOR);
 define('WS', '/');
@@ -10,36 +9,30 @@ define('ROOT', __DIR__ . DS);
 $dir = basename(ROOT);
 $tabUrl = explode($dir, $_SERVER['REQUEST_URI']);
 if (count($tabUrl) > 1) {
-	define('WEBROOT', $tabUrl[0] . $dir . WS);
+    define('WEBROOT', $tabUrl[0] . $dir . WS);
 } else {
-	define('WEBROOT', WS);
+    define('WEBROOT', WS);
 }
 
 require './vendor/autoload.php';
 
 $requete = new Requete();
 
-Routeur::add((new Route('GET', '', function() {
-	Echo "index<br>" .
-	"<a href=\"" . Routeur::getUrl('bonjour', ['name' => 'Fabien']) . "\">bonjour Fabien</a><br>" .
-	"<a href=\"" . Routeur::getUrl('user_do', ['action' => 'edit', 'id' => 95]) . "\">Edition de l'utilisateur n°95</a><br>" .
-	"<a href=\"" . Routeur::getUrl('contact') . "\">contactez moi</a><br>";
-}))->setName('home'));
+Routeur::get('/', function() {
+    Echo "index<br>" .
+    "<a href=\"" . Routeur::getUrl('bonjour', ['name' => 'Fabien']) . "\">bonjour Fabien</a><br>" .
+    "<a href=\"" . Routeur::getUrl('user_do', ['action' => 'edit', 'id' => 95]) . "\">Edition de l'utilisateur n°95</a><br>" .
+    "<a href=\"" . Routeur::getUrl('contact') . "\">contactez moi</a><br>";
+}, 'home');
 
-Routeur::add((new Route('GET', 'helo/{name}', function($name) {
-	Echo "bonjour $name<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
-}))->setName('bonjour'));
+Routeur::get('helo/{name}', function($name) {
+    Echo "bonjour $name<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
+}, 'bonjour');
 
-Routeur::add(
-		(new Route('GET', 'user/{action}/{id}', function($id, $action) {
-			Echo "l'utilisateur n°$id fait $action<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
-		}))
-				->setName('user_do')
-				->setValidation('id', '[0-9]+')
-				->setValidation('action', 'delete|edit')
-);
+Routeur::get('user/{action}/{id}', function($id, $action) {
+    Echo "l'utilisateur n°$id fait $action<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
+}, 'user_do')->setValidation('id', '[0-9]+')->setValidation('action', 'delete|edit');
 
-Routeur::add((new Route('GET', 'contact', 'Page@about'))->setName('contact'));
+Routeur::get('contact', 'ControleurPage@about', 'contact');
 
-Routeur::reparti($requete);
-
+Routeur::reparti($requete->client['METHODE'], $requete->client['URI']);
