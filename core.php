7274f7c4c63a0d592a9399d66dc96da7e3d11a2e
fzed51 <fzed51@yahoo.fr>
2015-05-18 23:57:50 +0200

    :up: Ajout d'une nouvelle route 'mapControleur'
    modification inconnue dans Route

--- core.php
+++ core.php
@@ -9,9 +9,9 @@ define('ROOT', __DIR__ . DS);
 $dir = basename(ROOT);
 $tabUrl = explode($dir, $_SERVER['REQUEST_URI']);
 if (count($tabUrl) > 1) {
-    define('WEBROOT', $tabUrl[0] . $dir . WS);
+	define('WEBROOT', $tabUrl[0] . $dir . WS);
 } else {
-    define('WEBROOT', WS);
+	define('WEBROOT', WS);
 }
 
 require './vendor/autoload.php';
@@ -19,19 +19,21 @@ require './vendor/autoload.php';
 $requete = new Requete();
 
 Routeur::get('/', function() {
-    Echo "index<br>" .
-    "<a href=\"" . Routeur::getUrl('bonjour', ['name' => 'Fabien']) . "\">bonjour Fabien</a><br>" .
-    "<a href=\"" . Routeur::getUrl('user_do', ['action' => 'edit', 'id' => 95]) . "\">Edition de l'utilisateur n°95</a><br>" .
-    "<a href=\"" . Routeur::getUrl('contact') . "\">contactez moi</a><br>";
+	Echo "index<br>" .
+	"<a href=\"" . Routeur::getUrl('bonjour', ['name' => 'Fabien']) . "\">bonjour Fabien</a><br>" .
+	"<a href=\"" . Routeur::getUrl('user_do', ['action' => 'edit', 'id' => 95]) . "\">Edition de l'utilisateur n°95</a><br>" .
+	"<a href=\"" . Routeur::getUrl('contact') . "\">contactez moi</a><br>";
 }, 'home');
 
 Routeur::get('helo/{name}', function($name) {
-    Echo "bonjour $name<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
+	Echo "bonjour $name<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
 }, 'bonjour');
 
 Routeur::get('user/{action}/{id}', function($id, $action) {
-    Echo "l'utilisateur n° $id fait $action<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
+	Echo "l'utilisateur n° $id fait $action<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
 }, 'user_do')->setValidation('id', '[0-9]+')->setValidation('action', 'delete|edit');
+
+Routeur::mapControleur('App\Controleur\News');
 
 Routeur::get('contact', 'ControleurPage@about', 'contact');
 
