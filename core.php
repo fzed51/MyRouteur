0ee2f970d0d48131e65a40a19fbbfdbcb0994cc9
fzed51 <fzed51@users.noreply.github.com>
2015-05-18 17:36:57 +0200

    :+1: création d'une clase PDO simple

--- core.php
+++ core.php
@@ -30,7 +30,7 @@ Routeur::get('helo/{name}', function($name) {
 }, 'bonjour');
 
 Routeur::get('user/{action}/{id}', function($id, $action) {
-    Echo "l'utilisateur n° $id fait $action<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
+    Echo "l'utilisateur nÂ° $id fait $action<br><a href=\"" . Routeur::getUrl('home') . "\">Home</a>";
 }, 'user_do')->setValidation('id', '[0-9]+')->setValidation('action', 'delete|edit');
 
 Routeur::get('contact', 'ControleurPage@about', 'contact');
