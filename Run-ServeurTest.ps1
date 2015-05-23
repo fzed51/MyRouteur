9180ccb74b1d1cf8898c0c0421ad25fbfa96f87d
fzed51 <fzed51@yahoo.fr>
2015-05-23 13:53:33 +0200

    :+1: Script pour lancer un serveur web PHP

---
+++ Run-ServeurTest.ps1
@@ -0,0 +1,15 @@
+Param(
+    [switch]$test
+)
+
+$webRoot = ([string]$MyInvocation.MyCommand.Path).Replace("\"+[string]$MyInvocation.MyCommand.Name, '')
+
+if(-not $test){
+    $webRoot = Join-Path -Path $webRoot -ChildPath '.'
+    php -S 127.0.0.2:8765 -t "$webRoot"
+    &"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "127.0.0.2:8765/index.php"
+} else {
+&"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "127.0.0.2:8765/test.php"
+
+    php -S 127.0.0.2:8765 -t "$webRoot" -e -n
+}    
