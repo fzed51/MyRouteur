64fcadc9294cfd5d03bc0057fba27846f7c9d967
fzed51 <fzed51@yahoo.fr>
2015-05-31 21:50:24 +0200

    :up: lancemant de chrome avant le serveur
    si non chrome ne se lance qu'apres arret du serveur

--- Run-ServeurTest.ps1
+++ Run-ServeurTest.ps1
@@ -6,9 +6,9 @@ $webRoot = ([string]$MyInvocation.MyCommand.Path).Replace("\"+[string]$MyInvocat
 
 if(-not $test){
     $webRoot = Join-Path -Path $webRoot -ChildPath '.'
-    php -S 127.0.0.2:8765 -t "$webRoot"
     &"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "127.0.0.2:8765/index.php"
+    php -S 127.0.0.2:8765 -t "$webRoot" "serveur-routeur.php"
 } else {
-	php -S 127.0.0.2:8765 -t "$webRoot"
 	&"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "127.0.0.2:8765/test.php"    
+	php -S 127.0.0.2:8765 -t "$webRoot"
 }    
