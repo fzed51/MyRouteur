33a4d24b8668aade8556efe675e5698edfc72320
fzed51 <fzed51@users.noreply.github.com>
2015-06-03 16:46:11 +0200

    :up: ecriture des news

--- page/news/read.php
+++ page/news/read.php
@@ -4,6 +4,7 @@
     <a href="<?= Routeur::getUrl('News.index'); ?>">&Lt;</a>
     <h2><?= $new->titre; ?>
         <a class="bouton" href="<?= Routeur::getUrl('News.Update', ['id' => $new->id]); ?>">&lt;edit&gt;</a>
+        <a class="bouton" href="<?= Routeur::getUrl('News.Update', ['id' => $new->id]); ?>">&lt;edit&gt;</a>
     </h2>
     <p><?= $new->text; ?></p>
 </div>
